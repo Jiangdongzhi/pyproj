6a0e30714dd3bab4a6cf6b3c8f53ecef84f08909
Dorian <dorian@hz.cn>
2018-10-09 07:31:48 +0800

    merge files

--- excelreader.py
+++ excelreader.py
@@ -1,12 +1,15 @@
 import xlrd
 
-data = xlrd.open_workbook('E:\\Dorian\\plant.xlsx')
-sheet_name = "Machine"
+data = xlrd.open_workbook('E:\\cfd\\cfd.xlsx')
+sheet_name = "Sheet1"
 
 table = data.sheets()[0]
 table1 = data.sheet_by_name(sheet_name)
 
+start_date = datetime.strptime('2018-06-11', '%Y-%m-%d')
 print (table1.nrows)
-
+print (table1.ncols)
 print (table1.cell(4,4))
+
+
 
